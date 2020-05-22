5d0800499a179dc99ca51bec3e7743fabae432af
Shreyas Patil <shreyaspatilg@gmail.com>
2020-05-22 15:06:43 +0530

    Code cleanup

--- app/src/test/java/dev/shreyaspatil/stateflow/example/ui/main/MainViewModelTest.kt
+++ app/src/test/java/dev/shreyaspatil/stateflow/example/ui/main/MainViewModelTest.kt
@@ -5,11 +5,6 @@ import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 
-/**M
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @ExperimentalCoroutinesApi
 class MainViewModelTest {
 
